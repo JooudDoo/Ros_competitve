13e99da313b12f13eee1a6bb75afd7f38894c1f6
JooudDoo <asegor1@ya.ru>
2023-12-10 12:45:04 +0700

    Аннотирование

--- autorace_core_TheRosBoss/module/logger.py
+++ autorace_core_TheRosBoss/module/logger.py
@@ -6,6 +6,24 @@ previous_message_id = -1
 previous_messages = deque(maxlen=LOGGING_POOL_SIZE)
 
 def log_info(node, message, debug_level, msg_id=-1, allow_repeat=False):
+    """
+    
+        Простенький фильтр логов
+        
+        node         - узел рос, который ведет логгирование  
+
+        message      - текст сообщения  
+
+        debug_level  - минимальный уровень дебага для отображения сообщения  
+
+        msg_id       - айди сообщения для отброса дублей (на случай если у нас сообщения не повторяются текстом, но смыслом)  
+
+            Если равно -1, то единицой идентификации становиться сам текст сообщения  
+
+        allow_repeat - разрешить выводить несколько раз подряд одинаковое сообщение  
+
+
+    """
     global previous_message_id, previous_messages
 
     if DEBUG_LEVEL < debug_level:
