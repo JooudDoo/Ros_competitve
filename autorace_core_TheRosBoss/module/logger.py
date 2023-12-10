799b04ae36091132cabee486510a7fd051fee891
JooudDoo <asegor1@ya.ru>
2023-12-10 12:20:07 +0700

    Фикс логгирования

--- autorace_core_TheRosBoss/module/logger.py
+++ autorace_core_TheRosBoss/module/logger.py
@@ -1,9 +1,9 @@
 from collections import deque
 
-from module.config import DEBUG_LEVEL, LOGGIN_POOL_SIZE
+from module.config import DEBUG_LEVEL, LOGGING_POOL_SIZE
 
 previous_message_id = -1
-previous_messages = deque(maxlen=LOGGIN_POOL_SIZE)
+previous_messages = deque(maxlen=LOGGING_POOL_SIZE)
 
 def log_info(node, message, debug_level, msg_id=-1, allow_repeat=False):
     global previous_message_id, previous_messages
@@ -12,6 +12,8 @@ def log_info(node, message, debug_level, msg_id=-1, allow_repeat=False):
         return
 
     if not allow_repeat and msg_id == -1 and message in previous_messages:
+        if previous_messages[-1] != message:
+            previous_messages.append(message)
         return
 
     if not allow_repeat and previous_message_id == msg_id and msg_id != -1:
